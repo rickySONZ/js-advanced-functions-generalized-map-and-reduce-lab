e9c718bc206e53f238028ccd57f3dc680a181f95
Ryan Erricson <ryanerricson@gmail.com>
2021-05-13 16:19:55 -0400

    commit tree 1bed0f8ace5a506e83acf6f90cf511867e62b3e0

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Add your functions here
+function map(array, func) {
+    const newArray = []
+    for (const i in array){
+        newArray.push(func(array[i]))
+    }
+    return newArray
+}
+
+function reduce(sourceArray, func, startingPoint){
+
+    let i;
+
+    if(!startingPoint){
+        startingPoint = sourceArray[0]
+        i = 1
+    } else{
+        i = 0
+    }
+
+    let x = startingPoint 
+
+    for (i; i < sourceArray.length; i++){
+        x = func(sourceArray[i], x)
+    }
+    return x
+   
+}
+
+// function reduce(sourceArray, func, startingPoint){
+//     let i;
+//     if(!startingPoint){
+//       startingPoint = sourceArray[0];
+//      i =1;
+//     }
+//     else{
+//         i=0;
+//     }
+//     let x=startingPoint;
+  
+//     for (i; i < sourceArray.length; i++) {
+//       x = func(sourceArray[i], x)
+//     }
+  
+//     return x;
+//   }
